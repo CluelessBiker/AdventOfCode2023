5f7ac8a5a62b91c17bd097955007086aaa881e58
Lauren-Nicole <popich.red@gmail.com>
2023-12-02 15:08:10 +0000

    feat(challenge1Part1.js): solution to challenge

---
+++ challenges/challenge1Part1.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const listArray = fs.readFileSync('docs/day1.txt', 'utf-8').trim().split('\n')
+
+
+const arrayOfNum = (array) => {
+  const banana = array.map(str => str.replace(/[^0-9]/g, ""));
+  return banana
+}
+
+// console.log(arrayOfNum(listArray))
+const numArray = arrayOfNum(listArray)
+
+const codeNums = (array) => {
+  return array.map(num => {
+    return num[0] + num[num.length -1]
+  })
+}
+
+// console.log(codeNums(numArray))
+const codeDigits = codeNums(numArray)
+
+const finalCode = (array) => {
+  return array.map(num => Number(num)).reduce((acc, num) => acc + num, 0)
+}
+
+console.log(finalCode(codeDigits))
+// 54597
