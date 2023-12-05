5363717eefa1ea4047a978ba9f3473740513cace
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2023-12-05 09:15:17 +0200

    fix(day03Pt2): re-write file to resolve error

--- 2023/day03/day03Pt2.js
+++ 2023/day03/day03Pt2.js
@@ -1,61 +1,54 @@
 const fs = require('fs');
 
-const lines = fs.readFileSync('./input.txt', 'utf-8').trim().split('\n');
+fs.readFile('input.txt', 'utf-8', (err, data) => {
+	const lines = data.split('\n').filter((n) => n);
+	let rows = lines.length;
+	let cols = lines[0].length;
 
-/**
- * Credit for this challenge goes to
- * https://www.youtube.com/@frontend-coder
- * as I struggled with the concept,
- * but wished to complete the challenge as a learning exercise
- */
+	const gears = {};
 
-const rows = lines.length;
-const cols = lines[0].length;
+	const findGears = (str, num, i, j) => {
+		j = j === -1 ? 0 : j;
+		for (let k = 0; k < str.length; k++) {
+			const ch = str.charAt(k);
+			if (ch === '*') {
+				const ind = `${i}-${j + k}`;
+				gears[ind] = gears[ind]
+					? [...gears[ind], parseInt(num)]
+					: [parseInt(num)];
+			}
+		}
+	};
 
-let gears = {};
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			const n = '' + lines[i][j];
+			if (isNaN(n)) continue;
 
-const findGears = (string, num, i, j) => {
-	j = j === -1 ? 0 : j;
+			let num = n;
+			while (++j < cols) {
+				if (Number.isInteger(parseInt(lines[i][j]))) num += lines[i][j];
+				else break;
+			}
 
-	for (let k = 0; string.length; k++) {
-		const char = string.charAt(k);
-		if (char === '*') {
-			const ind = `${i}-${j + k}`;
-			gears[ind] = gears[ind]
-				? [...gears[ind], parseInt(num)]
-				: [parseInt(num)];
+			const top =
+				i === 0 ? '' : lines[i - 1].substring(j - num.length - 1, j + 1);
+			const bottom =
+				i === rows - 1 ? '' : lines[i + 1].substring(j - num.length - 1, j + 1);
+			const left = lines[i][j - num.length - 1] || '';
+			const right = lines[i][j] || '';
+
+			findGears(top, num, i - 1, j - num.length - 1);
+			findGears(bottom, num, i + 1, j - num.length - 1);
+			findGears(left, num, i, j - num.length - 1);
+			findGears(right, num, i, j);
 		}
 	}
-};
 
-for (let i = 0; i < rows; i++) {
-	for (let j = 0; j < cols; j++) {
-		const n = '' + lines[i][j];
-		if (isNaN(n)) continue;
+	const tally = Object.values(gears)
+		.filter((x) => x.length === 2)
+		.map((x) => x[0] * x[1])
+		.reduce((a, x) => a + x, 0);
 
-		let num = n;
-		while (++j < cols) {
-			if (Number.isInteger(parseInt(lines[i][j]))) num += lines[i][j];
-			else break;
-		}
-
-		const top =
-			i === 0 ? '' : lines[i - 1].substring(j - num.length - 1, j + 1);
-		const bottom =
-			i === rows - 1 ? '' : lines[i + 1].substring(j - num.length - 1, j + 1);
-		const left = lines[i][j - num.length - 1] || '';
-		const right = lines[i][j] || '';
-
-		findGears(top, num, i - 1, num.length - 1);
-		findGears(bottom, num, i + 1, j - num.length - 1);
-		findGears(left, num, i, j - num.length - 1);
-		findGears(right, num, i, j);
-	}
-}
-
-const tally = Object.values(gears)
-	.filter((it) => it.length === 2)
-	.map((it) => it[0] * it[1])
-	.reduce((acc, num) => acc + num, 0);
-// console.log(tally);
-console.log(gears);
+	console.log(tally);
+});
