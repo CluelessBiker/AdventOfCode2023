f900a1c7c3572a3f8ce431d82bb8ec2fca886c22
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2023-12-06 16:07:35 +0200

    fix(day04Pt2): remove dead code & implement correct funct

--- 2023/day04/day04Pt2.js
+++ 2023/day04/day04Pt2.js
@@ -33,41 +33,22 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
 
 	const allMatches = lines.map((it) => matchingNumbers(it));
 
-	/**
-	 * Function based on my original understanding of Pt.2
-	 * This logic is ultimately incorrect.
-	 * Logic is intended to tally up the score of the subsequesnt cards
-	 * index: 2 / count: 4 => it will calculate the total of
-	 * the values in positions 3, 4, 5, 6 of the array.
-	 */
-	// const calculateCards = allMatches.map((it) => it === 0 ? 0 : Math.pow(2, it - 1));
-	// const sumOfSubsequent = (count, index) => {
-	// 	const startIndex = index + 1;
-	// 	const endIndex = startIndex + count - 1;
-	// 	if (startIndex >= calculateCards.length) {
-	// 		return 0;
-	// 	}
-	// 	const selectedNumbers = calculateCards.slice(startIndex, endIndex + 1);
-	// 	return sum = selectedNumbers.reduce((acc, num) => acc + num, 0);
-	// }
-
-	// const bananaWaffle = allMatches.map((num, index) => sumOfSubsequent(num, index))
-
-	/**
-	 * Second function also incorrect.
-	 * Once again, have not understood what is being asked.
-	 */
 	for (let i = 0; i < allMatches.length; i++) {
 		const matches = allMatches[i];
 		const startIndex = i + 1;
-		const endIndex = startIndex + matches - 1;
 
-		for (let j = startIndex; j <= endIndex && j < cardCount.length; j++) {
-			cardCount[j]++;
+		if (matches > 0) {
+			for (
+				let j = startIndex;
+				j < startIndex + matches && j < cardCount.length;
+				j++
+			) {
+				if (cardCount[j]) {
+					cardCount[j] += cardCount[i] || 0;
+				}
+			}
 		}
 	}
-
-	console.log(cardCount);
 
 	const tally = cardCount.map((x) => x).reduce((a, x) => a + x, 0);
 	// console.log(tally);
